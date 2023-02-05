dff38c75ea0e5ea571462b2a332bacb76aca4616
Konyaev Alexey <a.konyaev@tinkoff.ru>
2023-02-05 20:46:48 +0300

    up zeebe version

--- local-start/set_zeebe.sh
+++ local-start/set_zeebe.sh
@@ -3,5 +3,5 @@ export APP=zeebe-app
 
 export ZOOKEEPER_VERSION=3.4.6
 export KAFKA_VERSION=2.13-2.8.1
-export ZEEBE_VERSION=8.0.4
+export ZEEBE_VERSION=8.1.6
 export ELASTICSEARCH_VERSION=7.16.2  # !? 8.1.2 - operate not working with this version
